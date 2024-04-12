6cf433fa5f715e07f92014c6e8ea78fe6e58cccb
DrRoot85 <mhbmis@icloud.com>
2024-04-13 01:55:32 +0300

    Update with statuscmd :)

--- config.mk
+++ config.mk
@@ -7,10 +7,10 @@ VERSION = 6.5
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/local/include
-X11LIB = /usr/local/lib
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
 
-BDINC = /usr/local/include/fribidi
+BDINC = /usr/include/fribidi
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -18,7 +18,7 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/local/include/freetype2
+FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 #MANPREFIX = ${PREFIX}/man
