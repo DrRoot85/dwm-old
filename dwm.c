c05ded1dcaaa41f4c6f4f4238ad72c1a284967a8
DrRoot85 <mhbmis@icloud.com>
2024-04-16 13:13:17 +0300

    add fakefullscreen

--- dwm.c
+++ dwm.c
@@ -202,6 +202,7 @@ typedef struct {
 	int monitor;
 } Rule;
 
+
 typedef struct Systray   Systray;
 struct Systray {
 	Window win;
@@ -792,7 +793,8 @@ clientmessage(XEvent *e)
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
 		|| cme->data.l[2] == netatom[NetWMFullscreen])
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
-				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
+				// || (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
+			|| cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
 
         if (cme->data.l[1] == netatom[NetWMSticky]
                 || cme->data.l[2] == netatom[NetWMSticky])
@@ -826,7 +828,7 @@ void
 configurenotify(XEvent *e)
 {
 	Monitor *m;
-	Client *c;
+	// Client *c;
 	XConfigureEvent *ev = &e->xconfigure;
 	int dirty;
 
@@ -839,9 +841,9 @@ configurenotify(XEvent *e)
 			drw_resize(drw, sw, bh);
 			updatebars();
 			for (m = mons; m; m = m->next) {
-				for (c = m->clients; c; c = c->next)
-					if (c->isfullscreen)
-						resizeclient(c, m->mx, m->my, m->mw, m->mh);
+				// for (c = m->clients; c; c = c->next)
+				// 	if (c->isfullscreen)
+				// 		resizeclient(c, m->mx, m->my, m->mw, m->mh);
 				resizebarwin(m);
 			}
 			focus(NULL);
@@ -1635,8 +1637,8 @@ movemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
-	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
-		return;
+	// if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
+	// 	return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1927,8 +1929,8 @@ resizemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
-	if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
-		return;
+	// if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
+	// 	return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -2246,24 +2248,24 @@ setfullscreen(Client *c, int fullscreen)
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
 		c->isfullscreen = 1;
-		c->oldstate = c->isfloating;
-		c->oldbw = c->bw;
-		c->bw = 0;
-		c->isfloating = 1;
-		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
-		XRaiseWindow(dpy, c->win);
+		// c->oldstate = c->isfloating;
+		// c->oldbw = c->bw;
+		// c->bw = 0;
+		// c->isfloating = 1;
+		// resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+		// XRaiseWindow(dpy, c->win);
 	} else if (!fullscreen && c->isfullscreen){
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)0, 0);
 		c->isfullscreen = 0;
-		c->isfloating = c->oldstate;
-		c->bw = c->oldbw;
-		c->x = c->oldx;
-		c->y = c->oldy;
-		c->w = c->oldw;
-		c->h = c->oldh;
-		resizeclient(c, c->x, c->y, c->w, c->h);
-		arrange(c->mon);
+		// c->isfloating = c->oldstate;
+		// c->bw = c->oldbw;
+		// c->x = c->oldx;
+		// c->y = c->oldy;
+		// c->w = c->oldw;
+		// c->h = c->oldh;
+		// resizeclient(c, c->x, c->y, c->w, c->h);
+		// arrange(c->mon);
 	}
 }
 
@@ -2444,7 +2446,8 @@ showhide(Client *c)
 	if (ISVISIBLE(c)) {
 		/* show clients top down */
 		XMoveWindow(dpy, c->win, c->x, c->y);
-		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
+		// if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
+		if (!c->mon->lt[c->mon->sellt]->arrange || c->isfloating)
 			resize(c, c->x, c->y, c->w, c->h, 0);
 		showhide(c->snext);
 	} else {
@@ -2558,8 +2561,8 @@ togglefloating(const Arg *arg)
 {
 	if (!selmon->sel)
 		return;
-	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
-		return;
+	// if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
+	// 	return;
 	selmon->sel->isfloating = !selmon->sel->isfloating || selmon->sel->isfixed;
 	if (selmon->sel->isfloating)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
