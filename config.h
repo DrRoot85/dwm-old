4cc52ef4a6ac6a89273b8754e390ccaa67603591
DrRoot85 <mhbmis@icloud.com>
2024-04-18 15:38:12 +0300

    adding  barHeight

--- config.h
+++ config.h
@@ -17,6 +17,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 // static char font[]            = { "Hack:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" , "terminus:style=Bold:size=10" ,"monospace:size=10" };
 static const char *fonts[]          = { "Hack:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" , "terminus:style=Bold:size=10" ,"monospace:size=10" };
 // static char dmenufont[]       = { "Hack:size=12:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true", "monospace:size=12" };
